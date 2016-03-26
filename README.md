fa56837e40b19d6e3213c309e29f3e1256ad2480
Savvinov Aleksandr <savvinovan@gmail.com>
2016-03-26 17:37:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Office 365
+
+1. Added README.md
+1. TDD
+2. Parallax
+3. Фоточки
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var stylus        = require('gulp-stylus');
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+    gulp.watch("./app/stylus/*.styl", ['stylus']);
+    gulp.watch("./app/*.html", browserSync.reload)
+})
+
+gulp.task('default', ['stylus','browser-sync'])
+
+gulp.task('stylus', function () {
+  return gulp.src('./app/stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
+});
